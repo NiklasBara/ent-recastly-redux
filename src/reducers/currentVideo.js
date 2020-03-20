5eed1073969501efe6c84c70489f04b62efbf427
SomeRandomDev747 <Rochan747@gmail.com>
2020-03-20 16:43:12 +0100

    changed reducers to use spread operators for copiing new state (immutable)

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -7,7 +7,7 @@ const initialState = exampleVideoData[0];
 
 const currentVideoReducer = (state = initialState, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
-  return action.type === 'CHANGE_CURRENT_VIDEO' ? action.payload : state;
+  return action.type === 'CHANGE_CURRENT_VIDEO' ? {...action.payload} : state;
 };
 
 export default currentVideoReducer;
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -4,8 +4,7 @@ import exampleVideoData from '../data/exampleVideoData'
 
 const initialState = exampleVideoData;
 var videoListReducer = (state = initialState, action) => {
-  console.log(action);
-  return action.type === `CHANGE_VIDEO_LIST` ? action.payload : state;
+  return action.type === `CHANGE_VIDEO_LIST` ? [...action.payload] : state;
 };
 
 export default videoListReducer;
